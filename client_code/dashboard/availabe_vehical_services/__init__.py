d82f7533b9122eeec9e45d90b3259c5ff875f057
rathourpankaj1999@gmail.com <rathourpankaj1999@gmail.com>
2024-05-02 10:00:33 +0000

    Edited servicers.servicers_dashboard.ItemTemplate1, dashboard.available_zym_address and dashboard.availabe_vehical_services

--- client_code/dashboard/availabe_vehical_services/__init__.py
+++ client_code/dashboard/availabe_vehical_services/__init__.py
@@ -1,34 +1,27 @@
 from ._anvil_designer import availabe_vehical_servicesTemplate
 from anvil import *
 import anvil.server
-from ._anvil_designer import services_listTemplate
-from anvil import *
-import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from .. import servicer_id
 
 class availabe_vehical_services(availabe_vehical_servicesTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.user_id = user_id
-    # Any code you write here will run before the form opens.
-    data = app_tables.oxiclinics.search(serviceProvider_id =self.id)
-    if not data:
-        alert("No bookings yet on this date")
+    data = app_tables.oxiwheels.Search()
+    
 
     customers_list =[]
     for row in data:
-      customer_details =app_tables.users.get(id=row['user_id'])
+      customer_details =app_tables.oxiwheels.get(id=row['id'])
       customer = {}
-      customer["name"]=customer_details['Oxiclinics_Name']
+      customer["name"]=customer_details['Oxigyms_Name']
       customer["email"]=customer_details['email']
       customer["phone"]=customer_details['phone']
-      customer["slot_time"]=row['book_time']
+      # customer["slot_time"]=row['book_time']
       customer["address"]=row['address']
-      customer["image"] = customer_details['profile']
-      customer["date"] = row['book_date']
+      # customer["image"] = customer_details['profile']
+      # customer["date"] = row['book_date']
       customers_list.append(customer)
     self.repeating_panel_1.items=customers_list
--- client_code/dashboard/available_zym_address/__init__.py
+++ client_code/dashboard/available_zym_address/__init__.py
@@ -1,7 +1,9 @@
 from ._anvil_designer import available_zym_addressTemplate
 from anvil import *
 import anvil.server
-
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 class available_zym_address(available_zym_addressTemplate):
   def __init__(self, **properties):
@@ -9,3 +11,19 @@ class available_zym_address(available_zym_addressTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    data = app_tables.oxigyms.Search()
+    
+
+    customers_list =[]
+    for row in data:
+      customer_details =app_tables.oxigyms.get(id=row['id'])
+      customer = {}
+      customer["name"]=customer_details['Oxigyms_Name']
+      customer["email"]=customer_details['email']
+      customer["phone"]=customer_details['phone']
+      # customer["slot_time"]=row['book_time']
+      customer["address"]=row['address']
+      # customer["image"] = customer_details['profile']
+      # customer["date"] = row['book_date']
+      customers_list.append(customer)
+    self.repeating_panel_1.items=customers_list
