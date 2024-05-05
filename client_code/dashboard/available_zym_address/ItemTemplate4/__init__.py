9f0dfa60b4816b128a57712954c9531f403b7c98
rathourpankaj1999@gmail.com <rathourpankaj1999@gmail.com>
2024-05-05 16:08:13 +0000

    Created dashboard.aviailable_Hospital_Address.ItemTemplate5; Edited dashboard.availabe_vehical_services.ItemTemplate2, dashboard.available_zym_address, dashboard.available_zym_address.ItemTemplate4, dashboard.aviailable_Hospital_Address and 1 more; Deleted dashboard.available_zym_address.ItemTemplate3

--- client_code/dashboard/available_zym_address/ItemTemplate4/__init__.py
+++ client_code/dashboard/available_zym_address/ItemTemplate4/__init__.py
@@ -12,3 +12,7 @@ class ItemTemplate4(ItemTemplate4Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_3_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('slot_book')
--- client_code/dashboard/available_zym_address/__init__.py
+++ client_code/dashboard/available_zym_address/__init__.py
@@ -1,33 +1,33 @@
-# from ._anvil_designer import available_zym_addressTemplate
-# from anvil import *
-# import anvil.server
-# import anvil.tables as tables
-# import anvil.tables.query as q
-# from anvil.tables import app_tables
+from ._anvil_designer import available_zym_addressTemplate
+from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
-# class available_zym_address(available_zym_addressTemplate):
-#   def __init__(self, **properties):
-#     # Set Form properties and Data Bindings.
-#     self.init_components(**properties)
+class available_zym_address(available_zym_addressTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
 
-#     # Any code you write here will run before the form opens.
-#     data = app_tables.oxigyms.search()
+    # Any code you write here will run before the form opens.
+    data = app_tables.oxigyms.search()
     
 
-#     customers_list =[]
-#     for row in data:
-#       customer_details =app_tables.oxigyms.get(id=row['id'])
-#       customer = {}
-#       customer["name"]=customer_details['Oxigyms_Name']
-#       customer["email"]=customer_details['email']
-#       customer["phone"]=customer_details['phone']
-#       # customer["slot_time"]=row['book_time']
-#       customer["address"]=row['address']
-#       # customer["image"] = customer_details['profile']
-#       # customer["date"] = row['book_date']
-#       customers_list.append(customer)
-#     self.repeating_panel_2.items=customers_list
+    customers_list =[]
+    for row in data:
+      customer_details =app_tables.oxigyms.get(id=row['id'])
+      customer = {}
+      customer["name"]=customer_details['Oxigyms_Name']
+      customer["email"]=customer_details['email']
+      customer["phone"]=customer_details['phone']
+      # customer["slot_time"]=row['book_time']
+      customer["address"]=row['address']
+      # customer["image"] = customer_details['profile']
+      # customer["date"] = row['book_date']
+      customers_list.append(customer)
+    self.repeating_panel_2.items=customers_list
 
-#   def button_2_click(self, **event_args):
-#     """This method is called when the button is clicked"""
-#     open_form('dashboard')
+  def button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('dashboard')
